6faaebbf1f0494ca48a9fb148937b4b428850470
Johan2511 <johanpenuelah@gmail.com>
2024-04-10 16:11:26 -0500

    Canvas definicion de tamaño,elementos y responsive

--- game.js
+++ game.js
@@ -1,1 +1,52 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+let canvasSize;
+let elementSize;
+
+
+
+function setCanvasSize() {
+    
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.75;
+    } else {
+        canvasSize = window.innerHeight * 0.75;
+    }
+    
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+    
+    elementSize = canvasSize / 10;
+    
+    startGame();
+    
+}
+
+// window.innerHeight
+// window.innerWidth 
+
+// game.fillRect(0, 50, 100, 100);
+// game.clearRect(0, 50, 50, 50);
+// game.clearRect(0, 0, 50, 50);
+// game.clearRect(0, 0, 50, 50);
+
+// game.font = '25px verdana';
+// game.fillStyle = 'purple';
+// game.textAlign = 'end';
+// game.fillText('Platzi', 50, 50)
+
+function startGame() {
+    console.log({canvasSize, elementSize});;
+
+    game.font = elementSize + 'px Verdana';
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'],elementSize * i, elementSize);
+        
+    }
+}
